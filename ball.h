e7012511580905b904e909d0ac3238e1c8cadf78
polms <robin.moalic@yahoo.fr>
2018-12-29 18:09:49 +0100

    Doc

--- ball.h
+++ ball.h
@@ -4,27 +4,100 @@
 #include <QPoint>
 #include <QPainter>
 
+/** \brief A ball
+ *
+ *  A ball to be draw on the screen. it can only be moved
+ *  by calling the nextPos() methods after setting an angle 
+ *  and a velocity.
+ *
+ *  It has a fixed diameter of 25 pixels.
+ */
 class Ball : public IDrawable
 {
 public:
+    /** \brief Constructor
+     *
+     *  The initPos and velocity parameters will be reused 
+     *  when calling reset()
+     *
+     *  \param initPos Where the ball starts
+     *  \param velocity initial speed of the ball
+     *  \param angle the initial angle of the ball
+     */
     Ball(QPointF initPos, double velocity, double angle);
+
+    /**
+     * \return The current position of the ball upper left corner
+     * (if you think of it as a square with rounded corner)
+     */
     QPointF getPos();
+    
+    /**
+     *  \return The current position of the ball center
+     */
     QPointF getPosCenter();
+    
+    /**
+     * \return The current velocity in pixel per seconds
+     */
     double getVelocity();
+
+    /**
+     * \return The next angle to be used when calling nextPos()
+     */
     double getAngle();
+
+    /**
+     * Set the next angle to be used when calling nextPos()
+     *
+     * \param angle the angle in radian
+     */
     void setAngle(double angle);
+
+    /**
+     * Place the ball to the location it was initialy placed on.
+     * With the initial angle but the current velocity.
+     */
     void reset();
+
+    /**
+     * Set the speed of the ball used for the nexts calls of nextPos()
+     *
+     * \param v speed in pixels per second
+     */
     void setVelocity(double v);
+
+    /**
+     *	Move the ball according to it's current speed and target angle.
+     *
+     *	To ensure the ball moves at the right speed, the caller must provide
+     *	the time elapsed betwin each call.
+     *
+     *  \param time_elapsed time in miliseconds
+     *	\todo store the time in the object itself to ensure the speed
+     */
     void nextPos(qint64 time_elapsed);
-    void setColor(QColor);
+
+    /**
+     * Set the color to draw the ball with
+     *
+     * \param color the color of the ball
+     */
+    void setColor(QColor color);
+
+    /**
+     * Draw the ball on the QPainter
+     *
+     * \param paint QPainter to draw on
+     */
     void draw(QPainter* paint);
 private:
-    QPointF initPos;
-    double initAngle;
-    QPointF pos;
-    double velocity;
-    double angle;
-    QColor color;
+    QPointF initPos;  /**< Starting position of the ball, used by reset() */
+    double initAngle; /**< Starting angle for the ball next position, used by reset() */
+    QPointF pos;      /**< Current position of the ball */
+    double velocity;  /**< Current velocity of the ball in pixels per second */
+    double angle;     /**< Current angle for the ball next position */
+    QColor color;     /** The color of the ball */
 };
 
 #endif // BALL_H
--- brique.h
+++ brique.h
@@ -7,20 +7,84 @@
 #include <QColor>
 #include <QPainter>
 
+/**
+ * A brick to be drawn on the screen for a break out 
+ * type of game. It has 3 states:
+ *  - new
+ *  - broken
+ *  - dead
+ * The brick changes states when using the -- operator.
+ * A new brick will be drawn as plain black square.
+ * A broken brick will have some texture on top of it.
+ * A dead brick is the same as the broken brick, the
+ * user must remove it from the game himself.
+ *
+ * The brick gets assign a value, this value can be 
+ * used for a scoring system for exemple.
+ */
 class Brique : public IDrawable
 {
 public:
-    Brique(double, double, int, int, int);
+    /**
+     * Constructor
+     * 
+     * \param pos_x the x axis to position the brick on
+     * \param pos_y the y axis to position the brick on
+     * \param size_w the width of the brick
+     * \param size_h the height of the brick
+     * \param value the value to assign at that brick
+     */
+    Brique(double pos_x, double pos_y, int size_w, int size_h, int value);
+
+    /**
+     * A Qt representation of the brick as a rectangle.
+     * It can be used to get the position and size of 
+     * the brick.
+     *
+     * \return The representation of the brick
+     */
     const QRectF getRepr();
+
+    /**
+     * \return The value of the brique
+     */
     int getValue();
+
+    /**
+     * A textual representation of the brick's death !!
+     *
+     * \deprecated They don't realy die =)
+     */
     virtual QString getDestroyedText();
+
+    /**
+     * Draw the brick on the Painter.
+     *
+     * \param paint The Painter to draw the brick on
+     */
     void draw(QPainter* paint);
+
+    /**
+     * \return true if the brick is in the new or broken state, false otherwise
+     */
     bool isAlive();
+
+    /**
+     * Change the state of the brick.
+     * new -> broken -> dead.
+     * Does nothing when the brick is dead.
+     */
     Brique operator--(int);
 protected:
-    QRectF repr;
-    int value;
-    int destroyed_state;
+    QRectF repr; /**< the brick representation (position and size) */
+    int value; /**< a value to be used for scoring type things */
+    int destroyed_state; /**< the current state holder 2=new, 1=broken, 0=dead */
+    /**
+     * The image to be drawn over the brick in the broken state.
+     * It is best if the image uses transparency to keep the color of the brick.
+     *
+     * \todo free the data
+     */
     const QImage* brique1 = loadImage(":/brique1.png");
 };
 
